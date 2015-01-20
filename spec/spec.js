f081a3ff250fad6915ad3e072569184d711960e2
Frank Sam Hu <fsh1991@hotmail.com>
2015-01-20 12:27:01 -0600

    Testing jasmine

--- spec/spec.js
+++ spec/spec.js
@@ -1,7 +1,24 @@
 describe("First jasmine project", function() {
-  it("should say hello jasmine", function() {
+	it("should say hello jasmine", function() {
+		expect(helloWorld()).toBe("hello world");
+	});
 
-    expect(helloWorld()).toBe("hello world");
-  });
+	it("should write datable on the page", function(){
+		expect(title).toBe("Learning unit testing jasmine");
+		expect(title).not.toBe("Knockout");
+	});
 
+	it("should include in the title Jasmine", function(){
+		expect(title).toMatch(/Jasmine/i);
+	});
+
+	it("verificacion de suma",function(){
+		expect(suma(2,2)).toBe(4);
+		expect(suma(3,6)).toBe(9);
+		expect(suma(-50,"1000")).toBe(950);
+		expect(suma(-50,"iefasd")).toBe(null);
+		expect(suma(4,8)).toBeGreaterThan(10);
+		expect(suma(80,80)).toBeLessThan(170);
+	});
 });
+
--- src/script.js
+++ src/script.js
@@ -1,3 +1,20 @@
+var title = "Learning unit testing jasmine";
+
 function helloWorld() {
   return "hello world";
 }
+
+function titulo(){
+	document.getElementById("titleDiv").innerHTML = title;
+};
+titulo();
+
+function suma(num1, num2){
+	if(isNaN(num1)||isNaN(num2)){
+		return null;
+	}else{
+		num1 = parseInt(num1);
+		num2 = parseInt(num2);
+		return num1+num2;
+	};
+};
